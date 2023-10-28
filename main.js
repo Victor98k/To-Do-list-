4f48327909a82e49adbad3d5e9a4a85c32a9cdc8
Victor98k <Victorgustav98@gmail.com>
2023-10-28 23:23:37 +0200

    first push

---
+++ main.js
@@ -0,0 +1,35 @@
+
+
+
+
+
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addTask() {
+  if (inputBox.value === '') {
+    alert("Please add your task!");
+  } else {
+    if (listContainer.childElementCount < 5) {
+      let li = document.createElement("li");
+      li.innerHTML = inputBox.value;
+      listContainer.appendChild(li);
+
+
+      let span = document.createElement("span");
+      span.innerHTML = "\u00d7";
+      li.appendChild(span);
+    } else {
+      alert("Damn G i think 5 is max for today");
+    }
+  }
+  inputBox.value = "";
+}
+
+listContainer.addEventListener("click", function (e) {
+  if (e.target.tagName === "LI") {
+    e.target.classList.toggle("checked");
+  } else if (e.target.tagName === "SPAN") {
+    e.target.parentElement.remove();
+  }
+}, false);
